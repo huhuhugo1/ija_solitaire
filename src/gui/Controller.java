2746c85c553c65fc7b64f11b52a2bc8a51ec0379
Jan Kubica <xkubic39@gmail.com>
2017-05-06 23:53:31 +0200

    Doplneno, nezobrazuji se karty.

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,4 +1,94 @@
 package gui;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.event.EventHandler;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.util.HashSet;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Jan Kubica
+ * @login xkubic39
+ */
+public class Controller implements Initializable {
+
+    @FXML
+    private ImageView gCardDeck; // fx:id="gCardDeck"
+    @FXML
+    private ImageView gPutDownPack; // fx:id="gPutDownPack"
+
+    /*
+        @FXML
+        private void handleButtonAction(ActionEvent event) {
+            System.out.println("Toto je cardDeck!");
+        }
+    */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+     //   gCardDeck.setImage(new Image("file:/../../lib/res/A(C).png"));
+
+        // click on gCardDeck object
+        gCardDeck.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("Juuuu, jeeee... CardDeck!");
+                //      if (gCardDeck.isVisible())
+                //          gCardDeck.setVisible(false);
+                event.consume();
+
+                gCardDeck.setImage(new Image("file:/../../lib/res/A(H).png"));
+            }
+        });
+
+        gPutDownPack.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+
+                gPutDownPack.setImage(new Image("file:/../../lib/res/A(C).png"));
+
+                System.out.println("Juuuu, jeeee... CardDeck!");
+                //   if (!gCardDeck.isVisible())
+                //      gCardDeck.setVisible(true);
+
+                event.consume();
+            }
+        });
+
+    }
+
 }
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -5,9 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
-
+/*
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
@@ -15,7 +16,20 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+*/
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
 
+        Scene scene = new Scene(root, 630, 480);
+        stage.setScene(scene);
+
+        stage.resizableProperty().setValue(Boolean.FALSE);
+        stage.setTitle("Solitaire 1.0");
+        stage.getIcons().add(new Image("file:/../../lib/res/icon128x128.png"));
+
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
