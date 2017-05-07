9888214571c3acc9f523a3be31da6ca6ba295af1
Jan Kubica <xkubic39@gmail.com>
2017-05-07 22:16:50 +0200

    MultiWindow in Main.

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,35 +1,57 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
 import javafx.scene.image.Image;
-import javafx.scene.control.*;
+import javafx.fxml.FXML;
+import logic.Game;
 
 public class Main extends Application {
-/*
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+    protected int games_count = 0;
+
+    @FXML
+    private void AnotherGameAction(ActionEvent event) {
+        //TODO
     }
-*/
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        Parent window_0 = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        Parent window_1 = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        Parent window_2 = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        Parent window_3 = FXMLLoader.load(getClass().getResource("gui.fxml"));
+
+        GridPane gridPane = new GridPane();
+        gridPane.setGridLinesVisible(true);
+        gridPane.setStyle("-fx-background-color: #000000;");
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+
+        gridPane.add(window_0, 0, 0);
+        gridPane.add(window_1, 1, 0);
+        gridPane.add(window_2, 0, 1);
+        gridPane.add(window_3, 1, 1);
 
         stage.resizableProperty().setValue(Boolean.FALSE);
         stage.setTitle("Solitaire 1.0");
-        stage.getIcons().add(new Image("file:/../../lib/res/icon128x128.png"));
+        stage.getIcons().add(new Image("/res/icon128x128.png"));
 
-        Scene scene = new Scene(root, 630, 480);
+        Scene scene = new Scene(gridPane, 1275, 955);
         stage.setScene(scene);
         stage.show();
+
+        /*
+        Scene scene = new Scene(window_0, 630, 480);
+        stage.setScene(scene);
+        stage.show();
+        */
+        games_count++;
     }
 
     public static void main(String[] args) {
